913e94cdfcca15b9597453cf2823446b4c3f4195
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-05 12:57:36 +0700

    TC04

---
+++ src/main/java/page_objects/BookTicketPage.java
@@ -0,0 +1,17 @@
+package page_objects;
+
+import elements.Label;
+import org.openqa.selenium.By;
+
+public class BookTicketPage extends GeneralPage {
+    private final Label lblBookTicket = new Label(By.cssSelector("#content>h1"));
+    private final Label lblBookTicketForm = new Label(By.cssSelector("fieldset>legend"));
+
+    public String getBookTicketTitle() {
+        return this.lblBookTicket.getText();
+    }
+
+    public String getLbLBookTicketForm() {
+        return this.lblBookTicketForm.getText();
+    }
+}
