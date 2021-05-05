e5de40c08cee91bd195564a95bda97ce530c2b45
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-05 17:52:44 +0700

    TC02

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,20 +7,18 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
     @BeforeMethod
     @Parameters("browser")
-    public void setup (@Optional("chrome") String browser) {
+    public void setup(@Optional("chrome") String browser) {
         Log.info("Pre-condition");
         BrowserHelper.startBrowser(BrowserHelper.DriverType.valueOf(browser.toUpperCase()));
         BrowserHelper.navigateToUrl(Constant.RAILWAY_URL);
     }
 
     @AfterMethod
-    public void closePage() {
+    public void afterMethod() {
         Log.info("Post-condition");
         BrowserHelper.quitBrowser();
     }
