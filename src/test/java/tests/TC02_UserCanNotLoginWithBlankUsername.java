023b30df522f776edbf19fb8553b3afbbf167d88
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-06 09:05:50 +0700

    fixed TC02

--- src/test/java/tests/TC02_UserCanNotLoginWithBlankUsername.java
+++ src/test/java/tests/TC02_UserCanNotLoginWithBlankUsername.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import page_objects.HomePage;
 import page_objects.LoginPage;
 
-public class TC02 extends TestBase {
+public class TC02_UserCanNotLoginWithBlankUsername extends TestBase {
     HomePage homePage = new HomePage();
     LoginPage loginPage = new LoginPage();
     String blank = "";
