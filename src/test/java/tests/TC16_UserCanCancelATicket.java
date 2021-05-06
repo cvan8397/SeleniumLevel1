fe39809f42a4e01ae9eba2c8a5395125c6339a38
caivan <caivan.8397@gmail.com>
2021-05-07 00:33:10 +0700

    TC16

---
+++ src/test/java/tests/TC16_UserCanCancelATicket.java
@@ -0,0 +1,32 @@
+package tests;
+
+import helper.DataHelper;
+import models.Account;
+import models.Ticket;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page_objects.*;
+
+public class TC16_UserCanCancelATicket extends TestBase {
+    HomePage homePage = new HomePage();
+    RegisterPage registerPage = new RegisterPage();
+    LoginPage loginPage = new LoginPage();
+    BookTicketPage bookTicketPage = new BookTicketPage();
+    MyTicketPage myTicketPage = new MyTicketPage();
+
+    @Test(description = "User can cancel a ticket")
+    public void TC016() {
+        Account account = new Account(DataHelper.getRandomEmail(), DataHelper.getRandomPassword(), DataHelper.getRandomPID());
+        homePage.gotoRegisterPage();
+        registerPage.createAccount(account);
+        registerPage.gotoLoginPage();
+        loginPage.login(account);
+        loginPage.gotoBookTicketPage();
+        Ticket ticket = new Ticket(DataHelper.getRandomDepartDate(), "Nha Trang", "Sài Gòn", "Soft seat", "1");
+        bookTicketPage.bookTicket(ticket);
+        bookTicketPage.gotoMyTicketPage();
+        myTicketPage.cancelTicket();
+
+        Assert.assertFalse(myTicketPage.isTicketDisplayed(), "Ticket is not deleted !");
+    }
+}
