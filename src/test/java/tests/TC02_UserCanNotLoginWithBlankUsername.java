c00bf5f2a53e3bb78a747b745d10c85884a77eb2
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-06 09:46:34 +0700

    fixed blank TC02

--- src/test/java/tests/TC02_UserCanNotLoginWithBlankUsername.java
+++ src/test/java/tests/TC02_UserCanNotLoginWithBlankUsername.java
@@ -9,12 +9,11 @@ import page_objects.LoginPage;
 
 public class TC02_UserCanNotLoginWithBlankUsername extends TestBase {
     HomePage homePage = new HomePage();
-    LoginPage loginPage = new LoginPage();
-    String blank = "";
+    LoginPage loginPage = new LoginPage();;
 
     @Test(description = "User can't login with blank 'Username' textbox")
     public void testCase02() {
-        Account account = new Account(blank, Constant.PASSWORD);
+        Account account = new Account("", Constant.PASSWORD);
         homePage.gotoLoginPage();
         loginPage.login(account);
 
