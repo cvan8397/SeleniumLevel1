e3f0e4bf0534ca0f4b130fea0af8fb9da1c8002b
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-06 14:05:13 +0700

    TC04 - User is redirected to Book ticket page after logging in (#6)
    * TC04
    
    * fixed TC04
    
    * fix TC04
    
    * update TC04

---
+++ src/test/java/tests/TC04_UserIsRedirectedToBookTicketPageAfterLoggingIn.java
@@ -0,0 +1,28 @@
+package tests;
+
+import helper.Constant;
+import models.Account;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page_objects.BookTicketPage;
+import page_objects.HomePage;
+import page_objects.LoginPage;
+
+public class TC04_UserIsRedirectedToBookTicketPageAfterLoggingIn extends TestBase {
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+    BookTicketPage bookTicketPage = new BookTicketPage();
+
+    @Test(description = "User is redirected to Book ticket page after logging in")
+    public void testCase04() {
+        Account account = new Account(Constant.USERNAME, Constant.PASSWORD);
+        homePage.gotoBookTicketPage();
+        loginPage.login(account);
+
+        String actualResult = bookTicketPage.getBookTicketTitle();
+        String expectedResult = "Book ticket";
+
+        Assert.assertEquals(actualResult, expectedResult, "Book ticket page is not displayed as expected");
+        Assert.assertEquals(bookTicketPage.getBookTicketFormTitle(), "Book ticket form", "Book ticket form is not displayed as expected");
+    }
+}
