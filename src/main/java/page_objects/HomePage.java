f000900288c50bf4eb23aaea9d330605f220b0cc
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-06 15:58:04 +0700

    Change name TC06

--- src/main/java/page_objects/HomePage.java
+++ src/main/java/page_objects/HomePage.java
@@ -4,9 +4,9 @@ import elements.Label;
 import org.openqa.selenium.By;
 
 public class HomePage extends GeneralPage {
-    private final Label lblWelcomeRailWay = new Label(By.cssSelector("#content>h1"));
+    private final Label lblHeader = new Label(By.cssSelector("#content>h1"));
 
-    public String getWelcomeRailWayHeader() {
-        return this.lblWelcomeRailWay.getText();
+    public String getHeader() {
+        return this.lblHeader.getText();
     }
 }
