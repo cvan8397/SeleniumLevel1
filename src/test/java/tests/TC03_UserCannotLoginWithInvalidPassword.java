01b31cf021d89b5ade81e7fe5273ad0f198e23b6
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-06 09:33:44 +0700

    fixed nameclass TC03

--- src/test/java/tests/TC03_UserCannotLoginWithInvalidPassword.java
+++ src/test/java/tests/TC03_UserCannotLoginWithInvalidPassword.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import page_objects.HomePage;
 import page_objects.LoginPage;
 
-public class TC03 extends TestBase {
+public class TC03_UserCannotLoginWithInvalidPassword extends TestBase {
     HomePage homePage = new HomePage();
     LoginPage loginPage = new LoginPage();
     String invalidPassword = "1234";
@@ -18,7 +18,7 @@ public class TC03 extends TestBase {
         homePage.gotoLoginPage();
         loginPage.login(account);
 
-        String actualResult = loginPage.getGeneralErrorMsg();
+        String actualResult = loginPage.getGeneralErrorMsgText();
         String expectedResult = "There was a problem with your login and/or errors exist in your form.";
 
         Assert.assertEquals(actualResult, expectedResult, "Error message is not displayed as expected");
