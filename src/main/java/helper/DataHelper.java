6a8cd684b4ac46aea1b1a10ac9f37f5a5d998cd7
CaiVan <82853805+cvan8397@users.noreply.github.com>
2021-05-07 14:35:27 +0700

    delete method getRandomDepartDate()

--- src/main/java/helper/DataHelper.java
+++ src/main/java/helper/DataHelper.java
@@ -4,7 +4,6 @@ import com.github.javafaker.Faker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class DataHelper {
     static Faker faker = new Faker();
@@ -21,17 +20,9 @@ public class DataHelper {
         return faker.numerify("##########");
     }
 
-    public static String getRandomDepartDate() {
-        Calendar cal = Calendar.getInstance();
-        int ranNum = ThreadLocalRandom.current().nextInt(4, 30);
-        cal.add(Calendar.DATE, ranNum);
-        return new SimpleDateFormat("M/d/yyyy").format(cal.getTime());
-    }
-
     public static String getDateFromToday(int num) {
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DATE, num);
         return new SimpleDateFormat("M/d/yyyy").format(cal.getTime());
     }
-
 }
